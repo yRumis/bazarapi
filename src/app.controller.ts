21c53a3c912d7c270cda83d1ebc4ee57d9fff778
ramon <ramon.silva109@gmail.com>
2025-04-25 01:57:00 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('app')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('hi')
+  getHi(): string {
+    return 'yo';
+  }
+}
